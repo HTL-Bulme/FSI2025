3c17cff0d6fe70c1b6407d0f90292b0de4d6565e
Eric <springer.eric.2007@gmail.com>
2025-04-10 15:41:34 +0200

    Update der CreateTeable File

--- DatabaseSchema/CreateTables.sql
+++ DatabaseSchema/CreateTables.sql
@@ -1,20 +1,103 @@
-/* Hier kommen alle Create Table Statements her
- Vorsicht: SQLite Datentypen sind beschränkt auf:
- https://sqlite.org/stricttables.html*/
-
-/*Tabellen erstellen (Ohne Fremdschlüsse)*/
-CREATE TABLE IF NOT EXISTS ITEM (
-    itemId TEXT PRIMARY KEY,
-    name TEXT,
-    itemLength INTEGER,
-    itemWidth INTEGER,
-    itemHeight INTEGER
-) STRICT;
-
 CREATE TABLE IF NOT EXISTS STOLOC (
-    stoLocId TEXT PRIMARY KEY,
+    stoLocId CHAR(18) PRIMARY KEY,
     pickSequence INTEGER
-) STRICT;
+);
 
-/*Fremdschlüssel hinzufügen*/
-ALTER TABLE ITEM ADD COLUMN pickingStoLoc TEXT REFERENCES STOLOC(stoLocId);
+CREATE TABLE IF NOT EXISTS CUSTOMER (
+    customerId CHAR(13) PRIMARY KEY,
+    allowedLuType CHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS ITEM (
+    itemId CHAR(12) PRIMARY KEY,
+    name CHAR(40),
+    'length' INTEGER,
+    width INTEGER,
+    height INTEGER,
+    pickingStoLoc CHAR(18),
+    FOREIGN KEY (pickingStoLoc) REFERENCES STOLOC(stoLocId)
+);
+
+CREATE TABLE IF NOT EXISTS USER (
+    userId CHAR(10) PRIMARY KEY,
+    name CHAR(40)
+);
+
+CREATE TABLE IF NOT EXISTS OUTBOUNDORDER (
+    orderId CHAR(10) PRIMARY KEY,
+    pickingOrderId INTEGER,
+    state TEXT CHECK(state IN ('NEU', 'AKTIV', 'FERTIG')),
+    customerId CHAR(13),
+    deliveryTime DATETIME,
+    FOREIGN KEY (customerId) REFERENCES CUSTOMER(customerId)
+);
+
+CREATE TABLE IF NOT EXISTS OBO_LINE (
+    orderId CHAR(10),
+    lineNumber INTEGER,
+    itemId CHAR(12),
+    orderAmount INTEGER,
+    deliveryAmount INTEGER,
+    PRIMARY KEY (orderId, lineNumber),
+    FOREIGN KEY (orderId) REFERENCES OUTBOUNDORDER(orderId),
+    FOREIGN KEY (itemId) REFERENCES ITEM(itemId)
+);
+
+CREATE TABLE IF NOT EXISTS PICKINGORDER (
+    pickingOrderId INTEGER PRIMARY KEY,
+    status TEXT CHECK(status IN ('NEU', 'AKTIV', 'KOMM', 'FERTIG')),
+    currentUser CHAR(10),
+    startTime DATETIME,
+    finishTime DATETIME,
+    customerId CHAR(13),
+    deliveryTime DATETIME,
+    FOREIGN KEY (currentUser) REFERENCES USER(userId),
+    FOREIGN KEY (customerId) REFERENCES CUSTOMER(customerId)
+);
+
+CREATE TABLE IF NOT EXISTS PICKINGORDER_LINE (
+    pickingOrderId INTEGER,
+    lineNumber INTEGER,
+    status TEXT CHECK(status IN ('NEU', 'AKTIV', 'KOMM', 'FERTIG')),
+    pickSequence INTEGER,
+    stoLocId CHAR(18),
+    itemId CHAR(12),
+    itemName CHAR(40),
+    orderAmount INTEGER,
+    bookingAmount INTEGER,
+    targetLuId CHAR(18),
+    targetStockObject INTEGER,
+    PRIMARY KEY (pickingOrderId, lineNumber),
+    FOREIGN KEY (pickingOrderId) REFERENCES PICKINGORDER(pickingOrderId),
+    FOREIGN KEY (stoLocId) REFERENCES STOLOC(stoLocId),
+    FOREIGN KEY (itemId) REFERENCES ITEM(itemId)
+);
+
+CREATE TABLE IF NOT EXISTS LOADINGUNIT (
+    luId CHAR(18) PRIMARY KEY,
+    stoLocId CHAR(18),
+    luType CHAR(10),
+    FOREIGN KEY (stoLocId) REFERENCES STOLOC(stoLocId)
+);
+
+CREATE TABLE IF NOT EXISTS STOCKOBJECT (
+    luId CHAR(18),
+    stockObjectNumber INTEGER,
+    itemId CHAR(12),
+    itemName CHAR(40),
+    amount INTEGER,
+    PRIMARY KEY (luId, stockObjectNumber),
+    FOREIGN KEY (luId) REFERENCES LOADINGUNIT(luId),
+    FOREIGN KEY (itemId) REFERENCES ITEM(itemId)
+);
+
+CREATE TABLE IF NOT EXISTS PERFTRACK (
+    perfTrackId INTEGER PRIMARY KEY,
+    trackTime DATETIME,
+    pickOrderId INTEGER,
+    userId CHAR(10),
+    pickCount INTEGER,
+    duration INTEGER,
+    FOREIGN KEY (pickOrderId) REFERENCES PICKINGORDER(pickingOrderId),
+    FOREIGN KEY (userId) REFERENCES USER(userId)
+);
